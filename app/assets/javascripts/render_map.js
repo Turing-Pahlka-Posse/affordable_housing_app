d19eacc67ed36f4aa28e7c5da11a0e3fab9215b9
July Tran <julyytran@gmail.com>
2016-05-21 14:21:16 -0600

    style neighborhoods layer

--- app/assets/javascripts/render_map.js
+++ app/assets/javascripts/render_map.js
@@ -1,44 +1,53 @@
 $(document).ready(function () {
-  renderMap();
+  renderBaseMap();
   toggleNeighborhoods();
 });
 
-function renderMap() {
+function renderBaseMap() {
   var baseLayer = L.mapbox.styleLayer('mapbox://styles/julyytran/ciohh5muy0010bpneqm1tg1ij').addTo(map);
-  // var neighborhoodsLayer = L.mapbox.featureLayer(neighborhoods).addTo(map);
-  // toggleNeighborhoods(neighborhoodsLayer)
 }
 
 function toggleNeighborhoods() {
+  var neighborhoodsLayer = L.geoJson(neighborhoods, {
+    style: getStyle
+  });
+  addLayer(neighborhoodsLayer, 'Neighborhoods', 1);
+}
+
+function getStyle() {
+  return {
+    weight: 2,
+    opacity: 0.8,
+    color: '#3887be',
+    fillOpacity: 0.35,
+    fillColor: '#82E899'
+   };
+}
+
+function addLayer(layer, name, zIndex) {
+  layer
+  .setZIndex(zIndex)
+  .addTo(map);
+
   var layers = document.getElementById('menu-ui');
 
-  addLayer(L.mapbox.featureLayer(neighborhoods), 'View Neighborhoods', 1);
+  var link = document.createElement('a');
+  link.href = '#';
+  link.className = 'active';
+  link.innerHTML = name;
 
-  function addLayer(layer, name, zIndex) {
-    layer
-        .setZIndex(zIndex)
-        .addTo(map);
+  link.onclick = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
 
-    // Create a simple layer switcher that
-    // toggles layers on and off.
-    var link = document.createElement('a');
-        link.href = '#';
-        link.className = 'active';
-        link.innerHTML = name;
+    if (map.hasLayer(layer)) {
+      map.removeLayer(layer);
+      this.className = '';
+    } else {
+      map.addLayer(layer);
+      this.className = 'active';
+    }
+  };
 
-    link.onclick = function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        if (map.hasLayer(layer)) {
-            map.removeLayer(layer);
-            this.className = '';
-        } else {
-            map.addLayer(layer);
-            this.className = 'active';
-        }
-    };
-
-    layers.appendChild(link);
-}
+  layers.appendChild(link);
 }
