a823dae3555f521acd9c1a4600927ee7007a6d49
July Tran <julyytran@gmail.com>
2016-05-21 13:34:38 -0600

    add neighborhood layer

--- app/assets/javascripts/render_map.js
+++ app/assets/javascripts/render_map.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 });
 
 function renderMap() {
-  // var featureLayer = L.mapbox.featureLayer(usCoordinates)
-  //   .addTo(map);
-  L.mapbox.styleLayer('mapbox://styles/julyytran/ciohh5muy0010bpneqm1tg1ij').addTo(map);
+  var baseLayer = L.mapbox.styleLayer('mapbox://styles/julyytran/ciohh5muy0010bpneqm1tg1ij').addTo(map);
+  var neighborhoodsLayer = L.mapbox.featureLayer(neighborhoods).addTo(map);
 }
