e3063882ad98d62edc323a28a09141c0a6edb443
July Tran <julyytran@gmail.com>
2016-05-21 12:22:56 -0600

    add and style basic streets base map Base map from mapbox of whole world Auto zooms to denver

--- app/assets/javascripts/render_map.js
+++ app/assets/javascripts/render_map.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   renderMap();
 });
 
-function renderMap () {
-  var featureLayer = L.mapbox.featureLayer(usCoordinates)
-    .addTo(map);
+function renderMap() {
+  // var featureLayer = L.mapbox.featureLayer(usCoordinates)
+  //   .addTo(map);
+  L.mapbox.styleLayer('mapbox://styles/julyytran/ciohh5muy0010bpneqm1tg1ij').addTo(map);
 }
