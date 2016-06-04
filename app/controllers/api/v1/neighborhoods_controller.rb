577b5684b6ecda834a750cb310c21938b89bfe65
July Tran <julyytran@gmail.com>
2016-06-04 14:05:22 -0600

    takes in params with request

--- app/controllers/api/v1/neighborhoods_controller.rb
+++ app/controllers/api/v1/neighborhoods_controller.rb
@@ -4,11 +4,6 @@ module Api
       respond_to :json
 
       def index
-        params = {
-          "Address 1"=>"1510 Blake st. Denver CO",
-          "Address 2"=>"Elkhart Elementary Aurora CO",
-          "Address 3"=>"Denver Art Museum"
-        }
         @top_three = NeighborhoodAnalyst.top_three_neighborhoods(params)
         render json: @top_three
       end
